32063a40ea3b3bfda266f2dc7a4385ee0f16703b
Riqwan ThamirE <rmthamir@gmail.com>
2016-12-20 09:19:13 +0530

    added serializer, adapter, config routes

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.JSONAPIAdapter.extend({
+  namespace: 'api/v1',
+
+  pathForType: function(type) {
+    let underscored = Ember.String.underscore(type);
+
+    return Ember.String.pluralize(underscored);
+  }
+});
